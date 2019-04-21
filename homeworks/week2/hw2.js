8217bb8191db5c6286ae9ff8b62291fea0fee438
enter3017sky <enter3017sky@gmail.com>
2019-04-21 22:17:38 +0800

    update week2 & done

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,33 @@
 function capitalize(str) {
-  return str;
-}
+  const conventString = str.split(''); // 將字串使用 split 方法變成陣列
+  const result = []; // 宣告變數 result 為空陣列，將處理好的字元塞給它
 
+  // conventString 這個 Array，對它使用 forEach 方法，使用箭頭函式
+  conventString.forEach((item, index) => {
+    if (index === 0) { // 當 index === 0 時，當下的元素就轉成大寫，然後使用陣列的 push 方法新增到 result
+      result.push(item.toUpperCase());
+    } else { // 如果不是就直接 push 給 reuslt
+      result.push(item);
+    }
+  });
+
+  return result.join(''); // 將轉換好的 result Array 變原本的字串
+}
 console.log(capitalize('hello'));
+
+
+// https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+
+// 使用三元運算子
+// conventString.forEach((item, index) => {
+//   index === 0 ? result.push(item.toUpperCase()) : result.push(item)
+// })
+
+// 一開始寫的
+// conventString.forEach((item, index) => {
+//   if(index === 0 ) {
+//     result.push(conventString[index].toUpperCase())
+//   } else {
+//     result.push(conventString[index])
+//   }
+// })
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,23 @@
 function join(str, concatStr) {
-  return concatStr;
+  let result = '';
+  for (let i = 0; i < str.length; i + 1) {
+    if (i !== str.length - 1) {
+      result += str[i] + concatStr;
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
 }
 
 function repeat(str, times) {
-  return times;
+  let result = '';
+  for (let i = 0; i < times; i + 1) {
+    result += str;
+  }
+  return result;
 }
 
-console.log(join('a', '!'));
-console.log(repeat('a', 5));
+console.log(join([1, 2, 3], ''));
+
+console.log(repeat('yoyo', 2));
